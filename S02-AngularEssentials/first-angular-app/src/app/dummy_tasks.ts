d2d6ed54cc109073dcd809f9a9d01fc12b6f88b0
Vissamsetti krishna <srkvissamsetti@gmail.com>
2026-08-27 21:50:41 +0530

    Add Section 2 Angular Essentials project

---
+++ S02-AngularEssentials/first-angular-app/src/app/dummy_tasks.ts
@@ -0,0 +1,26 @@
+export const DUMMY_TASKS = [
+  {
+    id: 'u1',
+    task: 'Jasmine Washington'
+  },
+  {
+    id: 'u2',
+    task: 'Emily Thompson'
+  },
+  {
+    id: 'u3',
+    task: 'Marcus Johnson'
+  },
+  {
+    id: 'u4',
+    task: 'David Miller'
+  },
+  {
+    id: 'u5',
+    task: 'Priya Patel'
+  },
+  {
+    id: 'u6',
+    task: 'Arjun Singh'
+  },
+];
---
+++ S02-AngularEssentials/first-angular-app/src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output, output } from "@angular/core";
+import {type User} from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector:'app-user',
+  templateUrl:'./user.component.html',
+  styleUrl:'./user.component.css',
+  imports: [CardComponent]
+})
+export class UserComponent{
+  @Input({required:true}) user !: User;
+  @Input({required:true}) selected !: boolean;
+  @Output() select=new EventEmitter<string>();
+  //select=output<string>();
+
+
+  get imagePath(){
+    return 'assets/users/'+this.user.avatar;
+  }
+
+    onSelectUser(){
+      this.select.emit(this.user.id);
+    }
+}
